95c98ef79f3bd75b8030ecb61d962b36481e60c6
Álvaro López <ala114@alu.ua.es>
2025-03-01 17:32:47 +0100

    Creacion clases publicaciones, oferta y demanda
    Lo proximo:
    -validar que el rol de usuario sea correcto para poder crear cada una de las instancias
    -docstrings

---
+++ models/oferta.py
@@ -0,0 +1,12 @@
+from publicaciones import Publicacion
+
+class Oferta(Publicacion):
+    def __init__(self, titulo:str, descripcion:str, fecha_publicacion:str, usuario:str, precio:float, disponibilidad:bool)->None:
+        super().__init__(titulo, descripcion, fecha_publicacion, usuario)
+        self.precio = precio
+        self.disponibilidad = disponibilidad
+
+    def mostrar_informacion(self) -> str:
+        info_base = super().mostrar_informacion()
+        return f"""{info_base} \nPrecio: {self.precio}\nDisponibilidad: {self.disponibilidad}
+    """
