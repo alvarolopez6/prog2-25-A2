7a9f9e326831cbe5a446f8d25944775bebb937d2
Ismael-Escribano <156445579+Ismael902@users.noreply.github.com>
2025-04-04 10:25:39 +0200

    Initial CSVFIle commit
    Primeros pasos para implementar el modulo 'csv_file', es funcional pero aún con muchos problemas y falta de contenido

--- file_utils/csv_file.py
+++ file_utils/csv_file.py
@@ -6,7 +6,42 @@ Defines class 'CSVFile(File)' for reading and writing CSV files.
 Author: Ismael Escribano
 Creation Date: 29-03-2025
 """
+import csv
+
 from .file import File
 
 class CSVFile(File):
-    pass
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
+        self.data: list[list[str]] = []
+        self.headers: list[str] = []
+        # TODO: Comprobar que la ruta acaba en un archivo .csv, en caso contrario, hacerlo
+
+    def write_headers(self, fieldnames: list[str]) -> None:
+        with open(str(self.path), mode='w') as csv_file:
+            writer  = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            self.headers = fieldnames
+            writer.writeheader()
+
+    def write(self, content: list[str]) -> None:
+        if self.path.exists:
+            mode = 'a'
+        else:
+            mode = 'w'
+        if len(self.headers) > 0:
+            with open(str(self.path), mode=mode) as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=self.headers)
+                writer.writerow({'Nombre': content[0], 'Contraseña': content[1]})
+        else:
+            raise NotImplemented
+
+    def read(self) -> None:
+        self.data = []
+        with open(str(self.path), mode='r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            cont = 0
+            for row in csv_reader:
+                if cont == 0:
+                    cont += 1
+                else:
+                    self.data.append(row)
