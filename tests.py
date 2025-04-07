3d6acb3630ac28dad993012571ba84c48cdc4710
Álvaro López <ala114@alu.ua.es>
2025-04-07 18:26:20 +0200

    Single category update + DB decorator
    El decorador de la base de datos lo dejo comentado de momento hasta que esté habilitado

--- tests.py
+++ tests.py
@@ -1,8 +1,2 @@
-from generic_posts import *
-from offer import *
-from demand import *
-
-post1 = Offer('Title1','Description1','User1','Image1', 1)
-print(post1.display_information())
-print(type(post1.publication_date))
-print(type(post1.title))
+a = None
+print('A' if a is not None else 'b')
