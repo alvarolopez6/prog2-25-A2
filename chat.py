f94f06825f30d085282c5f8fe78a30aff00fe51c
Samrani205 <osef2@alu.ua.es>
2025-05-21 19:51:12 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,18 +1,31 @@
+import sqlite3
 
-
-
-
-
+class User:
+    pass
 
 class ChatUser:
-    ...
+    pass
+
+def database_decorator(func):
+    def wrapper(self, *args, **kwargs):
+        conn = sqlite3.connect('chats.db')
+        cursor = conn.cursor()
+        result = func(self, *args, **kwargs)
+        # Guardar el mensaje en la base de datos (REAL)
+        cursor.execute('''
+            INSERT INTO mensajes VALUES (?, ?, ?)
+        ''', (self.user1.username, self.user2.username, self.msg))
+        conn.commit()  # Confirmar cambios
+        conn.close()  # Cerrar conexión
+        return result
+    return wrapper
 
 
 class Chat(ChatUser):
-
     def __init__(self, user1: User, user2: User, msg: str) -> None:
-        self.user1: User = user1
-        self.user2: User = user2
-        self.msg: str = msg
-
-    
+        if not isinstance(user1,User) or not isinstance(user2,User) or not isinstance(msg,str):
+            raise TypeError('Incorrect type of arguments')
+        else:
+            self.user1 = user1
+            self.user2 = user2
+            self.msg = msg
